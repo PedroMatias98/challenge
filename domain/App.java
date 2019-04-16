822d0246d43ef6978d2274347cf123aaaab6bc91
Pedro Matias <pedro.matias.carvalho@tecnico.ulisboa.pt>
2019-04-16 21:16:45 +0100

    add 2 platforms and now it is possible to read a json file

--- domain/App.java
+++ domain/App.java
@@ -2,7 +2,19 @@ package domain;
 
 import java.util.Scanner;
 
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.util.Iterator;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileReader;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import java.util.List;
+import java.util.ArrayList;
+
 public class App {
+    private static List<Platform> platforms = new ArrayList<>();
 
     public static void helpCommand(){
       System.out.println("\nCommand:");
@@ -15,7 +27,54 @@ public class App {
       System.out.println("\tstatus - Summarizes data and displays it in a table-like fashion");
       System.out.println("\thelp - This screen");
     }
-    public static void main(String[] args){
+
+    @SuppressWarnings("unchecked")
+    public static void readJsonFileWithProperties(){
+      JSONParser parser = new JSONParser();
+      Object obj = null;
+      try{
+        obj = parser.parse(new FileReader("storedData/storedData.json"));
+      } catch(FileNotFoundException e){
+        System.out.println("inexistent file");
+      }
+        catch(ParseException e){
+          System.out.println("parse error");
+      }
+        catch(IOException e){
+          System.out.println("io error");
+      }
+        JSONObject jsonObject = (JSONObject) obj;
+
+        JSONArray platformList = (JSONArray) jsonObject.get("services");
+
+		  Iterator<JSONObject> iterator = platformList.iterator();
+        while(iterator.hasNext()){
+          jsonObject = iterator.next();
+          String id = (String) jsonObject.get("id");
+          String name = (String) jsonObject.get("name");
+          String statusUrl = (String) jsonObject.get("statusUrl");
+          String statusApiUrl = (String) jsonObject.get("statusApiUrl");
+          App.platforms.add(buildPlatform(id,name,statusUrl,statusApiUrl));
+        }
+    }
+
+    public static Platform buildPlatform(String id, String name, String statusUrl, String statusApiUrl){
+      Platform p = null;
+      if(id.equals("github"))
+        p = new Github(id,name,statusUrl,statusApiUrl);
+      if(id.equals("bitbucket"))
+        p = new Bitbucket(id,name,statusUrl,statusApiUrl);
+      return p;
+    }
+    
+	public static void main(String[] args){
+        App.readJsonFileWithProperties();
+        for(Platform p: platforms){
+          System.out.println(p.getId());
+          System.out.println(p.getName());
+          System.out.println(p.getStatusUrl());
+          System.out.println(p.getStatusApiUrl());
+        }
         Scanner scanner = new Scanner(System.in);
         while(true){
             String command = scanner.nextLine();
---
+++ domain/Github.java
@@ -0,0 +1,7 @@
+package domain;
+
+public class Github extends Platform {
+    public Github(String id, String name, String statusUrl, String statusApiUrl){
+        super(id,name,statusUrl,statusApiUrl);
+    }
+}
