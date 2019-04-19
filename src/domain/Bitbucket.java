766734550f41d577d4456b3b9a7c6eb19ee72ae9
Pedro Matias <pedro.matias.carvalho@tecnico.ulisboa.pt>
2019-04-19 22:15:42 +0100

    remove wrong files

--- src/domain/Bitbucket.java
+++
@@ -1,7 +0,0 @@
-package domain;
-
-public class Bitbucket extends Platform {
-    public Bitbucket(String id, String name, String statusUrl, String statusApiUrl){
-        super(id,name,statusUrl,statusApiUrl);
-    }
-}
